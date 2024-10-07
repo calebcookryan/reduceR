53590347e7df58704476bc8bcc143180411db103
“calebcookryan” <c9ryan@uwaterloo.ca>
2024-10-07 09:07:39 -0700

    updated code to reduce df
    -updated code
    -add plot code to visualize reads on plot

--- reduceR_mergeapproach.R
+++ reduceR_mergeapproach.R
@@ -40,25 +40,33 @@ reduceR_overlapping_rows <- function(df) {
   return(merged_df)
 }
 
-# Define the reference midnight datetime (January 1, 2012, at 00:00:00)
-midnight <- as.POSIXct("2012-01-01 00:00:00", format = "%Y-%m-%d %H:%M:%OS")
+# Define the reference midnight datetime (January 1, 2022, at 00:00:00)
+midnight <- as.POSIXct("2022-01-01 00:00:00", format = "%Y-%m-%d %H:%M:%OS")
 
 # Calculate the difference in seconds between each read and midnight (January 1, 2012)
 time_diff <- as.numeric(difftime(bz_readercapture_24_cleaned$reader_dt, midnight, units = "secs"))
+#get the millisecond values from the reader_time col
+time_diff_dec <- (bz_readercapture_24_cleaned$reader_time)
+# Extract the part after the decimal
+after_decimal <- sub(".*\\.(.*)", "\\1", time_diff)
+# Convert the extracted string into a numeric value
+after_decimal_numeric <- as.numeric(after_decimal)/1000
+#add the decimal values 
+full = time_diff + after_decimal_numeric
 
+time_diff = full
 # Add the calculated time difference as a new column
 bz_readercapture_24_cleaned$time_diff <- time_diff
 
 #set time_window for buffer zone around each read
-time_window = 0.1
+time_window = 0.3
 
 #select the data to be included in the network ##need to select as many cols as possible that arent duplicates (only one time col) 
 data<- bz_readercapture_24_cleaned %>%
-  select(reader_pit, genus, species,genus_species, sex, reader_site2 , time_diff,reader_year,repro_status, capture_year, correct_age,reader_evening_of) %>%
+  select(reader_pit, genus, species,genus_species, sex, spec.location, reader_site1, reader_site2 , reader_sitetype,reader_lat, reader_long, time_diff,reader_year,repro_status, capture_year, correct_age,reader_evening_of, reader_year) %>%
   mutate(start = time_diff - time_window) %>%
   mutate(end = time_diff + time_window) %>%
-  select(reader_pit, genus, species,genus_species, sex, reader_site2 ,reader_year,repro_status, capture_year, correct_age,reader_evening_of, start, end) 
-
+  select(reader_pit, genus, species,genus_species, sex, spec.location, reader_site1, reader_site2 , reader_sitetype,reader_lat, reader_long, time_diff,reader_year,repro_status, capture_year, correct_age,reader_evening_of, start,end, reader_year) 
 # Merge overlapping rows in the dataframe ##change this so that it is a single merged read, maybe dont edit or create a new col
 
 reduced_df <- reduceR_overlapping_rows(data)
@@ -74,7 +82,19 @@ head(bz_readercapture_24_cleaned$reader_dt_reconstructed)
 nrow(reduced_df)
 #proportion of reads removed following reduceR
  nrow(reduced_df)/nrow(data)
-
+#plot the grid of the new read groups surrounding each individual in time
+ grid_group_plot = reduced_df %>%
+   filter(reader_sitetype == "trail") %>% 
+   filter(species == "jamaicensis") %>%
+   filter(reader_year == "2024") %>%
+   filter(year(reader_evening_of)>=2023)
+ grid_group_plot = ggplot(grid_group_plot, aes(x = start, xend = end, y = reader_pit, yend = reader_pit, color = reader_site1)) +
+   geom_segment(size = 1) +
+   geom_point(aes(x = start), size = 3) +
+   geom_point(aes(x = end), size = 3) +
+   theme_minimal() +
+   labs(x = "Time (Start-End)", y = "Reader PIT", title = "Group Mapping by Time and Reader Site") +
+   theme(legend.title = element_text(face = "bold"))
 #save
 save(reduced_df, file = "reduced_df.RData")
 write.xlsx(reduced_df, file ="reduced_df.xlsx")
